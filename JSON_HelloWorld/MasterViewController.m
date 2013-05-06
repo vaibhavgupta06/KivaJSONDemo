8ee5c602cfb422ecd426114b65d2f5f495eca0d5
vaibhav <vaibhavgupta06@gmail.com>
2013-05-06 14:27:31 +0530

    Fetched models using JsonHTTPClient

--- JSON_HelloWorld/MasterViewController.m
+++ JSON_HelloWorld/MasterViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Vaibhav Gupta. All rights reserved.
 //
 
+#define kivaURL @"http://api.kivaws.org/v1/loans/search.json?status=fundraising"
+
 #import "MasterViewController.h"
 #import "JSONModelLib.h"
 #import "KivaFeed.h"
@@ -25,6 +27,19 @@
     [HUD showUIBlockingIndicatorWithText:@"Fetching JSON"];
     
     //fetch the feed
+    [JSONHTTPClient getJSONFromURLWithString:kivaURL completion:^(NSDictionary *json, JSONModelError *err) {
+        
+        if (err) {
+            NSLog(@"Some error occurred : %@",[err localizedDescription]);
+        }
+        
+        NSArray *loans;
+        loans = [LoanModel arrayOfModelsFromDictionaries:json[@"loans"]];
+        if (loans) {
+            NSLog(@"Loaded models successfully");
+            NSLog(@"The first loan is : %@",((LoanModel *)loans[0]).name);
+        }
+    }];
     
 }
 
